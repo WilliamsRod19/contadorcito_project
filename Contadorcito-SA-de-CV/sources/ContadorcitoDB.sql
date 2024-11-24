49e39b9b0f722a00bcdbe80f55366cf10380532d
Josue-Montoya <142867456+Josue-Montoya@users.noreply.github.com>
2024-11-24 01:55:33 -0600

    DB update

--- Contadorcito-SA-de-CV/sources/ContadorcitoDB.sql
+++ Contadorcito-SA-de-CV/sources/ContadorcitoDB.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE ContadorcitoDB;
+Create database ContadorcitoDB;
 USE ContadorcitoDB;
 
-CREATE TABLE Empresas (
+CREATE TABLE tbl_Empresas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     tipo ENUM('Natural', 'Jurídica') NOT NULL,
@@ -12,17 +12,38 @@ CREATE TABLE Empresas (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Usuarios (
+Create table tbl_Roles (
+	id_rol int auto_increment primary key,
+    nombre_rol varchar(50)
+);
+
+INSERT INTO tbl_Roles (nombre_rol) VALUES 
+('Administrador'),
+('Auxiliar');
+
+CREATE TABLE tbl_Usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    rol ENUM('Administrador', 'Auxiliar') NOT NULL,
+    usuario varchar(50),
+    clave VARCHAR(255) NOT NULL,
     activo BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id_rol int,
+    foreign key (id_rol) references tbl_Roles (id_rol)
 );
 
-CREATE TABLE Comprobantes_Compra (
+INSERT INTO tbl_Usuarios (nombre, email, usuario, clave, id_rol) VALUES 
+('Josue Montoya', 'admin@gmail.com', 'admin', '$2b$12$/DU5/JfI68JNrFcuY4kmI.kUu5v2tTlWfiCR6bfp7bN7hL0jOXgzy', 1),
+('Williams Gei', 'auxiliar@gmail.com', 'auxiliar', '$2b$12$/DU5/JfI68JNrFcuY4kmI.kUu5v2tTlWfiCR6bfp7bN7hL0jOXgzy', 2),
+('Carlos Gei', 'auxiliar2@gmail.com', 'auxiliar2', '$2b$12$/DU5/JfI68JNrFcuY4kmI.kUu5v2tTlWfiCR6bfp7bN7hL0jOXgzy', 2);
+
+SELECT u.id AS id_usuario, u.nombre, u.usuario, u.clave, r.nombre_rol 
+FROM tbl_Usuarios u 
+INNER JOIN tbl_Roles r ON u.id_rol = r.id_rol 
+WHERE u.usuario = 'admin';
+
+CREATE TABLE tbl_Comprobantes_Compra (
     id INT AUTO_INCREMENT PRIMARY KEY,
     empresa_id INT NOT NULL,
     tipo ENUM('Crédito Fiscal', 'Consumidor Final') NOT NULL,
@@ -34,11 +55,11 @@ CREATE TABLE Comprobantes_Compra (
     archivo_json VARCHAR(255),
     usuario_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (empresa_id) REFERENCES Empresas(id),
-    FOREIGN KEY (usuario_id) REFERENCES Usuarios(id)
+    FOREIGN KEY (empresa_id) REFERENCES tbl_Empresas(id),
+    FOREIGN KEY (usuario_id) REFERENCES tbl_Usuarios(id)
 );
 
-CREATE TABLE Comprobantes_Venta (
+CREATE TABLE tbl_Comprobantes_Venta (
     id INT AUTO_INCREMENT PRIMARY KEY,
     empresa_id INT NOT NULL,
     tipo ENUM('Crédito Fiscal', 'Consumidor Final') NOT NULL,
@@ -50,6 +71,6 @@ CREATE TABLE Comprobantes_Venta (
     archivo_json VARCHAR(255),
     usuario_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (empresa_id) REFERENCES Empresas(id),
-    FOREIGN KEY (usuario_id) REFERENCES Usuarios(id)
+    FOREIGN KEY (empresa_id) REFERENCES tbl_Empresas(id),
+    FOREIGN KEY (usuario_id) REFERENCES tbl_Usuarios(id)
 );
